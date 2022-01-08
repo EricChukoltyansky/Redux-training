ad70bae74ed728595daaa01dc47b20fb9d9a4e1c
EricChukoltyansky <kazohelmet@gmail.com>
2022-01-08 11:25:27 +0200

    first

---
+++ chuck/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <>
+    <App />
+  </>,
+  document.getElementById("root")
+);
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/EricChukoltyansky/Redux-training.git
+// git push -u origin main
